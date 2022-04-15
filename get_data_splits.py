201a1ce60f22a390a865b4a399901330317b9a3b
Nahuel Passano <passano43632@estudiantes.untref.edu.ar>
2022-04-14 21:29:16 -0300

    [Feat]: Added get_data_splits.py

---
+++ get_data_splits.py
@@ -0,0 +1,30 @@
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_dataset(data_path):
+    
+    with open(data_path, 'r') as fp:
+        data = json.load(fp)
+    
+    # extract inputs and targets
+    X = np.array(data['MFCCs'])
+    y = np.array(data['labels'])
+
+    return X,y
+
+def get_data_splits(data_path, test_size = 0.1, validation_size = 0.1):
+
+    # load dataset
+    X, y = load_dataset(data_path)
+
+    # create train/validation/test splits
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size)
+    X_train, X_validation, y_train, y_validation = train_test_split(X_test, y_test, test_size = validation_size)
+
+    # convert inputs from 2d to 3d arrays
+    X_train = X_train[..., np.newaxis]
+    X_validation = X_validation[..., np.newaxis]
+    X_test = X_test[..., np.newaxis]
+
+    return X_train, X_validation, X_test, y_train, y_validation, y_test
